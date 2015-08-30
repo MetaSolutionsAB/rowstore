10f7690fd5975a2b9ff344d9cad29f7c915f7241
Hannes Ebner <hannes@metasolutions.se>
2015-08-30 14:12:06 +0200

    updated license headers

--- webapp/src/main/java/org/entrystore/rowstore/store/RowStore.java
+++ webapp/src/main/java/org/entrystore/rowstore/store/RowStore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014 MetaSolutions AB <info@metasolutions.se>
+ * Copyright (c) 2011-2015 MetaSolutions AB <info@metasolutions.se>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
