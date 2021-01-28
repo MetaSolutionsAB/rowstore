9390b413a4108d6779dcd201a1778c15de761064
Hannes Ebner <hannes@metasolutions.se>
2021-01-28 12:52:27 +0100

    ROWS-36 #resolve Adding proper command line handling

--- standalone/common/src/main/java/org/entrystore/rowstore/RowStoreApplicationStandalone.java
+++ standalone/common/src/main/java/org/entrystore/rowstore/RowStoreApplicationStandalone.java
@@ -16,64 +16,167 @@
 
 package org.entrystore.rowstore;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PatternOptionBuilder;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.restlet.Application;
 import org.restlet.Component;
 import org.restlet.Context;
 import org.restlet.Server;
 import org.restlet.data.Protocol;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 public class RowStoreApplicationStandalone extends Application {
 
-    static Logger log = Logger.getLogger(RowStoreApplicationStandalone.class);
+    private final static org.slf4j.Logger log = LoggerFactory.getLogger(RowStoreApplicationStandalone.class);
+
+    public static String ENV_CONNECTOR_PARAMS = "ROWSTORE_CONNECTOR_PARAMS";
 
     public static void main(String[] args) {
-        int port = 8282;
-        URI configURI = null;
+        CommandLineParser parser = new DefaultParser();
+        Options options = new Options();
+        options.addOption(Option.builder("c").
+                longOpt("config").
+                required(System.getenv(RowStoreApplication.ENV_CONFIG_URI) == null).
+                desc("URL of configuration file, may be omitted if environment variable ROWSTORE_CONFIG_URI is set").
+                hasArg().
+                argName("URL").
+                optionalArg(false).
+                type(PatternOptionBuilder.URL_VALUE).
+                build());
+        options.addOption(Option.builder("p").
+                longOpt("port").
+                desc("port to listen on; default: 8282").
+                hasArg().
+                argName("PORT").
+                optionalArg(false).
+                type(PatternOptionBuilder.NUMBER_VALUE).
+                build());
+        options.addOption(Option.builder("l").
+                longOpt("log-level").
+                desc("log level, one of: ALL, TRACE, DEBUG, INFO, WARN, ERROR, FATAL, OFF; default: INFO").
+                hasArg().
+                argName("LEVEL").
+                optionalArg(false).
+                build());
+        options.addOption(Option.builder().
+                longOpt("connector-params").
+                desc("comma separated list of parameters to be used for the server connector, " +
+                        "the environment variable ROWSTORE_CONNECTOR_PARAMS may be used instead. " +
+                        "Example for Jetty: \"threadPool.minThreads=50,threadPool.maxThreads=250\"; " +
+                        "see the JavaDoc of JettyServerHelper for available parameters").
+                hasArg().
+                argName("SETTINGS").
+                optionalArg(false).
+                build());
+        options.addOption(Option.builder("h").longOpt("help").desc("display this help").build());
 
-        if (args.length > 0) {
-            configURI = new File(args[0]).toURI();
-            if (!new File(configURI).exists()) {
-                System.err.println("Configuration file not found: " + configURI);
-                configURI = null;
-            }
-        }
-
-        if (args.length > 1) {
-            try {
-                port = Integer.valueOf(args[1]);
-            } catch (NumberFormatException nfe) {
-                System.err.println(nfe.getMessage());
-            }
-        }
-
-        if (configURI == null && System.getenv(RowStoreApplication.ENV_CONFIG_URI) == null) {
-            System.out.println("RowStore - http://entrystore.org/rowstore/");
-            System.out.println("");
-            System.out.println("Usage: rowstore [path to configuration file] [listening port]");
-            System.out.println("");
-            System.out.println("Path to configuration file may be omitted only if environment variable ROWSTORE_CONFIG_URI is set to a URI. No other parameters must be provided if the configuration file is not provided as parameter.");
-            System.out.println("Default listening port is " + port + ".");
-
+        CommandLine cl = null;
+        try {
+            cl = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage() + "\n");
+            printHelp(options);
             System.exit(1);
         }
 
+        if (cl.hasOption("help")) {
+            printHelp(options);
+            System.exit(0);
+        }
+
+        String strPort = cl.getOptionValue("p", "8282");
+        int port = 8282;
+        try {
+            port = Integer.parseInt(strPort);
+        } catch (NumberFormatException nfe) {
+            System.err.println("Invalid port number, must be integer: " + strPort + "\n");
+            printHelp(options);
+            System.exit(1);
+        }
+
+        URI config = null;
+        try {
+            if (cl.hasOption("c")) {
+                config = new URI(cl.getOptionValue("c"));
+            } else {
+                config = new URI(System.getenv(RowStoreApplication.ENV_CONFIG_URI));
+            }
+        } catch (URISyntaxException e) {
+            System.err.println("Invalid configuration URL: " + e.getMessage() + "\n");
+            printHelp(options);
+            System.exit(1);
+        }
+
+        configureLogging(cl.getOptionValue("log-level", "INFO"));
+
         Component component = new Component();
         Server server = component.getServers().add(Protocol.HTTP, port);
+
+        String conParams;
+        if (cl.hasOption("connector-params")) {
+            conParams = cl.getOptionValue("connector-params");
+        } else {
+            conParams = System.getenv(ENV_CONNECTOR_PARAMS);
+        }
+        if (conParams != null) {
+            for (String param : conParams.split(",")) {
+                if (param.length() > 0) {
+                    String[] kv = param.split("=");
+                    if (kv.length == 2) {
+                        log.debug("Adding connector parameter: {}={}", kv[0], kv[1]);
+                        server.getContext().getParameters().add(kv[0].trim(), kv[1].trim());
+                    } else {
+                        System.err.println("Invalid connector parameter: " + param);
+                        System.exit(1);
+                    }
+                }
+            }
+        }
+
         server.getContext().getParameters().add("useForwardedForHeader", "true");
         component.getClients().add(Protocol.HTTP);
         component.getClients().add(Protocol.HTTPS);
         Context c = component.getContext().createChildContext();
 
         try {
-            component.getDefaultHost().attach(new RowStoreApplication(c, configURI));
+            component.getDefaultHost().attach(new RowStoreApplication(c, config));
             component.start();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private static void printHelp(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.setWidth(100);
+        formatter.setLeftPadding(2);
+        formatter.printHelp("rowstore", options,true);
+    }
+
+    private static void configureLogging(String logLevel) {
+        BasicConfigurator.configure();
+        Level l = Level.INFO;
+        if (logLevel != null) {
+            l = Level.toLevel(logLevel, Level.INFO);
+        }
+        Logger.getRootLogger().setLevel(l);
+        out("Log level set to " + l);
+    }
+
+    private static void out(String s) {
+        System.out.println(s);
     }
 
 }
