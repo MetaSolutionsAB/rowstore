55477dfe57b7fc863daabc9d8135aba8ae9edc8d
Hannes Ebner <hannes@metasolutions.se>
2021-02-03 16:55:37 +0100

    ROWS-80 #resolve Introducing new jvm parameter for status resource /status?jvm

--- webapp/src/main/java/org/entrystore/rowstore/resources/StatusResource.java
+++ webapp/src/main/java/org/entrystore/rowstore/resources/StatusResource.java
@@ -25,6 +25,8 @@ import org.restlet.resource.Get;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.management.ManagementFactory;
+
 /**
  * Returns status about the RowStore instance.
  *
@@ -32,16 +34,40 @@ import org.slf4j.LoggerFactory;
  */
 public class StatusResource extends BaseResource {
 
-	private static Logger log = LoggerFactory.getLogger(StatusResource.class);
+	private final static Logger log = LoggerFactory.getLogger(StatusResource.class);
 
 	@Get("json")
-	public Representation getJSON() throws JSONException {
+	public Representation getSimpleStatus() throws JSONException {
 		JSONObject result = new JSONObject();
 		result.put("service", "RowStore");
 		result.put("version", RowStoreApplication.getVersion());
 		result.put("datasets", getRowStore().getDatasets().amount());
 		result.put("activeEtlProcesses", getRowStore().getEtlProcessor().getActiveEtlProcesses());
 		return new JsonRepresentation(result);
+	}
+
+	@Get("json?jvm")
+	public Representation getJvmStatus() throws JSONException {
+		JSONObject jvm = new JSONObject();
+		jvm.put("totalMemory", Runtime.getRuntime().totalMemory());
+		jvm.put("freeMemory", Runtime.getRuntime().freeMemory());
+		jvm.put("maxMemory", Runtime.getRuntime().maxMemory());
+		jvm.put("availableProcessors", Runtime.getRuntime().availableProcessors());
+		jvm.put("totalCommittedMemory", getTotalCommittedMemory());
+		jvm.put("committedHeap", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getCommitted());
+		jvm.put("totalUsedMemory", getTotalUsedMemory());
+		jvm.put("usedHeap", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed());
+		return new JsonRepresentation(jvm);
+	}
+
+	long getTotalCommittedMemory() {
+		return ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getCommitted() +
+				ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getCommitted();
+	}
+
+	long getTotalUsedMemory() {
+		return ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() +
+				ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed();
 	}
 
 }
